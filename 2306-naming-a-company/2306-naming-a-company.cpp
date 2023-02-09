f99e421a6184a4902b66e423d2a17577a8e1a2ae
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-02-10 01:33:06 +0530

    Time: 741 ms (56.91%), Space: 83.4 MB (56.91%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
-        long long disName = 0;
-        vector<unordered_set<string>> arr(26);
-        for (string s : ideas) 
-            arr[s[0] - 'a'].insert(s.substr(1));
+        long long res = 0;
+        vector<unordered_set<string>> mp(26);
+        for(auto &s: ideas){
+            mp[s[0] - 'a'].insert(s.substr(1));
+        }
         
-        for (int i = 0; i < 25; i++) {
-            for (int j = i + 1; j < 26; j++) {
-                unordered_set<string> set;
-                set.insert(arr[i].begin(), arr[i].end());
-                set.insert(arr[j].begin(), arr[j].end());
-                disName += (arr[i].size() - set.size()) * (arr[j].size() - set.size());
+        int u1 = 0, u2 = 0;
+        for(int i = 0; i < 25; i++){
+            for(int j = i + 1; j < 26; j++){
+//                 compare every i and j
+                u1 = 0; u2 = 0;
+                for(auto &s: mp[i]){
+//                     check if exsists in j
+                    if(mp[j].find(s) == mp[j].end()){
+//                         not found
+                        u1++;
+                    }
+                }
+                for(auto &s: mp[j]){
+//                     check if exsists in j
+                    if(mp[i].find(s) == mp[i].end()){
+//                         not found
+                        u2++;
+                    }
+                }
+                
+                // cout << u1 << " " << u2 << endl;
+                res += u1 * u2;
+//                 u1 * u2 perms can be possible
             }
         }
-        return disName * 2;
+        return (2 * res);
     }
 };
