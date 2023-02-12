c46eb5469e1af05fdbfc0386bf8328370be991b7
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-02-12 22:53:15 +0530

    Time: 89 ms (98.11%), Space: 49.3 MB (86.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,47 +1,45 @@
-
-#define pb push_back
 class Solution {
 public:
-
-    bool isPart(string& s, int start, int end){
+    bool isPartition(int start, int end, string& s){
         while(start <= end){
             if(s[start] != s[end]){
                 return false;
-            }
+            }   
             start++;
             end--;
         }
-
         return true;
     }
-
-    void solve(int start, string &s, vector<string>& curr, vector<vector<string>>& res, int& n){
+    
+    
+    void solve(int start, vector<string>& curr, vector<vector<string>>& res, int& n, string &s){
         if(start == n){
-            res.pb(curr);
+//             end of the string
+            res.push_back(curr);
             return;
         }
-
-        // find all possible permutations
+        
         for(int i = start; i < n; i++){
-            // check the left from i is a partition or not
-            if(isPart(s, start, i)){
-                curr.pb(s.substr(start, i - start + 1));
-                solve(i + 1, s, curr, res, n);
+            if(isPartition(start, i, s) == true){
+//                 that part is partition, we can break into multiple pieces
+                curr.push_back(s.substr(start, i - start + 1));
+                solve(i + 1, curr, res, n, s);
                 curr.pop_back();
             }
         }
     }
-
+    
+    
     vector<vector<string>> partition(string s) {
-        int n = s.size();
-        vector<vector<string>> res;
-        if(n == 1){
-            res.pb({s});
-            return res;
+        int n = s.length();
+        if(n == 0){
+            return {{s}};
         }
-        int start = 0;
+        
         vector<string> curr;
-        solve(start, s, curr, res, n);
+        vector<vector<string>> res;
+        int start = 0;
+        solve(start, curr, res, n, s);
         return res;
     }
 };
