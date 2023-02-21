ba3190868f323a91695782b6422a4322133eebb5
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-02-21 11:01:54 +0530

    Time: 30 ms (55.07%), Space: 22.4 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,12 +5,32 @@ public:
         if(n == 1){
             return nums[0];
         }
-        for(int i = 0; i < n; i = i + 2){
-            if(nums[i] != nums[i + 1]){
-                return nums[i];
+        
+        int low = 0, high = n - 1, mid = 0;
+        while(low < high){
+            mid = (low + high) / 2;
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
+                return nums[mid];
+            }
+            if(mid % 2 == 0){
+//                 even
+                if(nums[mid] != nums[mid + 1]){
+//                     on the right
+                    high = mid;
+                }else{
+                    low = mid;  
+                }
+            }else{
+//                 odd
+                if(nums[mid] != nums[mid - 1]){
+//                     on the left
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
             }
         }
         
-        return nums[n-1];
+        return nums[low];
     }
 };
