e8987a6dd9f87c1d3d916d3f2c223eb96db40c65
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-02-22 15:29:08 +0530

    Time: 119 ms (7.48%), Space: 26.2 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    int left, right, curr, mid, d, ans;
+    int low, high, mid, ans, i, curr, d;
     
     int shipWithinDays(vector<int>& weights, int days) {
-//         creating n bin with proper bin size
-//         max size = sum(weights) in 1 day
-//         min size = max(weights) in some days
-//         so bounds are [max(weights), sum(weights)]
-        left = 0; right = 0;
+//         low = max(a) and high = sum(a)
+        low = 0;
+        high = 0;
         for(auto w: weights){
-            left = max(left, w);
-            right += w;
+            low = max(low, w);
+            high += w;
         }
-        // cout << left << " " << right << endl;
         
-        int i = 0, n = weights.size(), ans = INT_MAX;
+        int n = weights.size();
+        // cout << low << " " << high << endl;
         
-        while(left <= right){
+        while(low < high){
+            mid = low + (high - low) / 2;
             curr = 0;
             d = 1;
-            mid = left + (right - left) / 2;
-//             we are finding the number of partitions
             i = 0;
-            while(i < n && curr <= mid){
+            while(curr <= mid && i < n){
                 curr += weights[i];
                 i++;
                 if(curr > mid){
-//                     making a partition
-                    i--;
-                    curr = 0;
                     d++;
+                    curr = 0;
+                    i--;
                 }
             }
             
-            if(d > days){
-                left = mid + 1;
+            if(d <= days){
+//                 number of days if we choose this are less than given days, so we need to increase the days, or reduce the bucket size
+                high = mid;
             }else{
-//                 d <= days
-                ans = min(ans, mid);
-                right = mid - 1;
+//                 number of days are more, so we need to increase the bucket size
+                low = mid + 1;
             }
         }
         
-        return ans;
+        return low;
     }
 };
