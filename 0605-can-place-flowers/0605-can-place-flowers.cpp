e7a0d641bfd0cb5b9084b9b778e46b6bc428fe61
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-03-20 10:21:13 +0530

    Time: 16 ms (77.89%), Space: 20 MB (99.96%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,31 +1,64 @@
 class Solution {
 public:
+    void printVector(vector<int> v){
+        for(auto x: v){
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-//         solution involving counting of consequtive zereos
         if(n == 0){
-//             no need to place
             return true;
         }
-        
+        if(n > flowerbed.size()){
+            return false;
+        }
+        if(flowerbed.size() == 1 && n == 1 && flowerbed[0] == 0){
+            return true;
+        }
         for(int i = 0; i < flowerbed.size(); i++){
-//             check if the neighbouring states are empty
-//             3 major conditions
-//             should be 0
-//             on the left should be 0 or should be the first ele
-//             on the right should be 0 or should be the last element
-            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)){
+            if(flowerbed[i] == 0){
 //                 can place
-                n--;
-                if(n == 0){
-//                     nothing to place
-//                     so all are done and can return
-                    return true;
+//                 if((i == 0 && flowerbed[i + 1] == 0) || i == flowerbed.size() - 1 && flowerbed[i - 1] == 0 || (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)){
+// //                     can place now
+//                     flowerbed[i] = 1;
+//                     // printVector(flowerbed);
+//                     n--;
+//                     if(n == 0){
+//                         return true;
+//                     }
+//                 }
+                if(i == 0){
+                    if(flowerbed[i + 1] == 0){
+                        flowerbed[i] = 1;
+                        // printVector(flowerbed);
+                        n--;
+                        if(n == 0){
+                            return true;
+                        }
+                    }
+                }else if(i == flowerbed.size() - 1){
+                    if(flowerbed[i - 1] == 0){
+                        flowerbed[i] = 1;
+                        // printVector(flowerbed);
+                        n--;
+                        if(n == 0){
+                            return true;
+                        }
+                    }
+                }else{
+                    if(flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0){
+                        flowerbed[i] = 1;
+                        // printVector(flowerbed);
+                        n--;
+                        if(n == 0){
+                            return true;
+                        }
+                    }
                 }
-//                 place the flower now
-                flowerbed[i] = 1;
             }
         }
-        
         
         return false;
     }
