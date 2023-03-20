9454fe00b7d74ced92a0384df3bc59515968094e
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-03-20 17:10:13 +0530

    Time: 13 ms (61.77%), Space: 22.4 MB (9.73%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> bfs;
+    
+    void print2dVector(vector<vector<int>> v){
+        for(auto x: v){
+            for(auto y: x){
+                cout << y << " ";
+            }
+            cout << "" << endl;
+        }
+    }
+
+    void solve(TreeNode* root){
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        
+        vector<int> curr;
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            if(node == NULL){
+//                 level is ending
+                bfs.push_back(curr);
+                curr.clear();
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }else{
+//                 not null
+                curr.push_back(node->val);
+                if(node->left != NULL){
+                    q.push(node->left);
+                }
+                if(node->right != NULL){
+                    q.push(node->right);        
+                }
+            }
+        }
+    }
+    
+    int findBottomLeftValue(TreeNode* root) {
+        solve(root);
+        // print2dVector(bfs);
+        return bfs[bfs.size() - 1][0];
+        // return 0;
+    }
+};
