0609bf185a8e6d2867dc5478c89ae8e7bc67c43b
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-05-19 16:18:22 +0530

    Time: 6 ms (66.64%), Space: 14 MB (6.64%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int solve(vector<int>& cost, int start, vector<int>& v, int n){
+        if(start >= n){
+            return 0;
+        }
+        
+        if(v[start] != -1){
+            return v[start];
+        }
+        
+        // compute
+        // either 1 step or 2 steps
+        return v[start] = min(
+            cost[start] + solve(cost, start + 1, v, n),
+            cost[start] + solve(cost, start + 2, v, n)
+        ); 
+    
+    }
+    
+    int minCostClimbingStairs(vector<int>& cost) {
+        // we need to reah the top
+        
+        // conditions:
+        // 1. can start from index 0 or 1
+        // 2. can move either 1 steps or 2
+        
+        // similar to climbing stairs but given cost for each operation
+        
+        // so we need to add a final cost to move to last one
+        int n = cost.size();
+        
+        vector<int> v(n, -1);
+        v.push_back(0);
+        v[n-1] = cost[n-1];
+        
+        solve(cost, 0, v, n + 1);
+        return min(v[0], v[1]);
+    }
+};
+
+
+
