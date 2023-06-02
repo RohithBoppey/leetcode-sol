0356dc168694f8cefd2ccd0debd45ac2b4134719
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-02 10:49:11 +0530

    Time: 167 ms (54.09%), Space: 20.3 MB (38.44%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     
-    void print2dVector(vector<vector<int>> v){
-        for(auto &x: v){
-            for(auto &y: x){
-                cout << y << " ";
-            }
-            cout << endl;
-        }
-        cout << "" << endl;
-    }
-    
-    void printVector(vector<int> a){
-        for(auto x : a){
-            cout << x << " ";
-        }
-        cout << endl;
-    }
-    
     long long dis(int x1, int y1, int x2, int y2){
         return pow(x2-x1,2) + pow(y2-y1,2);
     }
     
     
     void dfs(int start, vector<vector<int>>& adjList, vector<int>& visited, int& len){
-        len++;
         visited[start] = 1;
         for(auto x: adjList[start]){
             if(!visited[x]){
                 // not visited neighbour
+                len++;
                 dfs(x, adjList, visited, len);
             }
         }
@@ -50,6 +33,10 @@ public:
                 // if the circles intersect, there is an edge
                 if(i != j){
                     long long distance = dis(bombs[i][0], bombs[i][1], bombs[j][0], bombs[j][1]);
+                    // since vice versa is not possible
+                    // a can intersect b, but cannot intersect a
+                    
+                    // condition -> r² > c1c2
                     if(distance <= pow(bombs[i][2], 2)){
                         // is valid
                         adjList[i].push_back(j);
@@ -73,7 +60,7 @@ public:
         for(int i = 0; i < n; i++){
             vector<int> visited(n, 0);
             // not visited
-            int len = 0;
+            int len = 1;
             dfs(i, adjList, visited, len);
             // once dfs is done
             maxCount = max(maxCount, len);
