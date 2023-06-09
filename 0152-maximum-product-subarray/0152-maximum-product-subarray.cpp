34a0f9300fb96dea5a7ddb9df26302f9785c3861
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-09 15:10:15 +0530

    Time: 12 ms (24.48%), Space: 13.7 MB (71.17%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int max1(int a, int b, int c){
+        return max(max(a, b), c);
+    }
+    
+    int min1(int a, int b, int c){
+        return min(min(a, b), c);
+    }
+    
+    int maxProduct(vector<int>& nums) {
+        int local_max = 1, local_min = 1, global_max = 0;
+        int n = nums.size();
+        
+        if(n == 1){
+            return nums[0]; 
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                local_max = 1;
+                local_min = 1;
+            }else{
+                int temp = local_max * nums[i];
+                local_max = max1(local_max * nums[i], local_min * nums[i], nums[i]);
+                local_min = min1(temp, local_min * nums[i], nums[i]);
+                
+                global_max = max(global_max, local_max);
+            }
+        }
+        
+        return global_max;
+    }
+};
