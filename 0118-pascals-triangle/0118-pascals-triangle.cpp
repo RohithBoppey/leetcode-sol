67a786d9bb1d1a6961a731646e89f097f171a7c1
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-24 19:38:22 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (75.71%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        // we need to generate a pascal traingle
+        
+        // given n
+        vector<vector<int>> res;
+        
+        if(n >= 1){
+            res.push_back({1});
+        }
+        
+        if(n >= 2){
+            res.push_back({1,1});
+            
+        }
+        
+        if(n <= 2) { return res; }
+        
+        // now we need to generate a pascal traingle
+        
+        vector<int> t;
+        int t1;
+        
+        for(int i = 2; i < n; i++){
+            t.clear();
+            t.push_back(1);
+            
+            // some kind of processing
+            
+            for(int j = 1; j <= i - 1; j++){
+                t1 = res[i - 1][j] + res[i - 1][j - 1];
+                t.push_back(t1);
+            }
+            
+            t.push_back(1);
+            res.push_back(t);
+        }
+        
+        return res;
+    }
+};
