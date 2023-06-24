180901d73d66c5dc2e23813c39210da6d8a8fe87
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-24 23:07:36 +0530

    Time: 70 ms (70.68%), Space: 39.8 MB (93.18%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    
+    int n;
+    int kadanesMax(vector<int>& nums){
+        // this function returns the max sum of an array in O(N)
+        int csum = 0;
+        int msum = nums[0];
+        
+        for(int i = 0; i < n; i++){
+            csum += nums[i];
+            msum = max(msum, csum);
+            csum = (csum < 0) ? 0 : csum;
+        }
+        
+        return msum;
+        
+        // O(N) SOLUTION
+    }
+    
+    int kadanesMin(vector<int>& nums){
+        // this function returns the min sum of an array in O(N)
+        int csum = nums[0];
+        int msum = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            csum = min(csum + nums[i], nums[i]);
+            msum = min(csum, msum);
+        }
+        
+        return msum;
+        
+        // O(N) SOLUTION
+    }
+    
+    int maxSubarraySumCircular(vector<int>& nums) {
+        n = nums.size();
+        int mn = INT_MAX, mx = INT_MIN;
+        mx = kadanesMax(nums);
+        
+        // finding the min sum
+        int total = 0;
+        
+        for(int i = 0; i < n; i++){
+            total += nums[i];
+        }
+        
+        // total sum is now populated
+        mn = kadanesMin(nums);
+        
+        cout << mx << " " << mn << endl;
+        
+        int csum = total - mn;
+        
+        return (mx > 0) ? max(mx, csum) : mx;
+    }
+};
