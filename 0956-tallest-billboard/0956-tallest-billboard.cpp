fe634a21e029a1e8a00523e3ca4ae308574d598d
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-24 18:23:46 +0530

    Time: 119 ms (48.90%), Space: 8.7 MB (78.85%) - LeetHub

---
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int n;
+    // the new dp is dp[index][diff] - max diss = -5000, so adding a threshold
+    int dp[21][10003];
+    
+    int solve(vector<int>& rods, int curr, int diff){
+        if(curr > n){
+            return 0;
+        }
+        
+        if(curr == n){
+            if(diff == 0){
+                return 0;
+            }
+            return INT_MIN;
+        }
+        
+        if(dp[curr][diff + 5000] != -1){
+            return dp[curr][diff + 5000];
+        }
+        
+        
+        // we can take or not take
+        int take1 = rods[curr] + solve(rods, curr + 1, diff + rods[curr]);
+        int take2 = rods[curr] + solve(rods, curr + 1, diff - rods[curr]);
+        int nottake = solve(rods, curr + 1, diff);
+        
+        return dp[curr][diff + 5000] = max({take1, take2, nottake});
+        
+    }
+    
+    int tallestBillboard(vector<int>& rods) {
+        n = rods.size();
+        memset(dp, -1, sizeof(dp));
+        // initial diff is 0
+        return solve(rods, 0, 0) / 2;
+    }
+};
