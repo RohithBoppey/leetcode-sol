eef991d0e9a3b8fc8d9b55d5480446333b99a5d3
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-28 15:19:07 +0530

    Time: 3 ms (84.88%), Space: 9.6 MB (22.58%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // find rows and cols
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        // since they are in order, we can use binary search
+        
+        // number of indices - [0, (rows*cols) - 1]
+        
+        int low = 0, high = (rows*cols) - 1;
+        int mid;
+        
+        int ri, ci;
+        
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            ri = mid / cols, ci = mid % cols;
+            if(matrix[ri][ci] == target){
+                // we found the element
+                cout << "found at row: " << ri << " col: " << ci << endl;
+                return true;
+            }else if(matrix[ri][ci] > target){
+                // reduce the high
+                high = mid - 1;
+            }else{
+                // matrix[ri][ci] < target
+                low = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+};
