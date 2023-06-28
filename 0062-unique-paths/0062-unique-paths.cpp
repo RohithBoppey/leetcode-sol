ad71558e2697f3b96fb05967e56c920c14410bb9
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-28 19:34:58 +0530

    Update 0062-unique-paths.cpp

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int uniquePaths(int rows, int cols) {
+        
+        // initialising since everything is possible
         vector<vector<int>> dp(rows, vector<int>(cols, 1));
+        
         for(int i = 1; i < rows; i++){
             for(int j = 1; j < cols; j++){
+                // processing based on both the ways possible
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
+        
         return dp[rows-1][cols-1];
     }
 };
