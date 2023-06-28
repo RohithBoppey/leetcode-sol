2b17cf855a3f3f814fc5513cadbc582f8adc5d34
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-28 22:51:22 +0530

    Time: 20 ms (25.95%), Space: 16 MB (29.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,42 +1,56 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& v) {
+    vector<int> majorityElement(vector<int>& nums) {
         vector<int> result;
-        int n = v.size(); //size of the array
-
-        int cnt1 = 0, cnt2 = 0; // counts
-        int el1 = INT_MIN; // element 1
-        int el2 = INT_MIN; // element 2
-
-        // applying the Extended Boyer Moore's Voting Algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt1 == 0 && el2 != v[i]) {
-                cnt1 = 1;
-                el1 = v[i];
+        int n = nums.size();
+        
+        // using the concept of mooore's but using 2 variables
+        // since max there can only be 2
+        
+        int c1 = 0, c2 = 0, e1 = INT_MIN, e2 = INT_MIN;
+        
+        for(int i = 0; i < n; i++){
+            // since both elements cant be equal
+            if(c1 == 0 && e2 != nums[i]){
+                c1 = 1;
+                e1 = nums[i];
+            }else if(c2 == 0 && e1 != nums[i]){
+                c2 = 1;
+                e2 = nums[i];
             }
-            else if (cnt2 == 0 && el1 != v[i]) {
-                cnt2 = 1;
-                el2 = v[i];
+            // now that c1 != 0 and c2 != 0
+            else if(e1 == nums[i]){
+                c1++;
             }
-            else if (v[i] == el1) cnt1++;
-            else if (v[i] == el2) cnt2++;
-            else {
-                cnt1--, cnt2--;
+            else if(e2 == nums[i]){
+                c2++;
+            }else{
+                c1--;
+                c2--;
             }
         }
-
-
-        // Manually check if the stored elements in
-        // el1 and el2 are the majority elements:
-        cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == el1) cnt1++;
-            if (v[i] == el2) cnt2++;
+        
+        // now that we found top repeating 2 elements, we are verifying
+        int t1 = 0, t2 = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(e1 == nums[i]){
+                t1++;
+            }
+            if(e2 == nums[i]){
+                t2++;
+            }
         }
-
-        int mini = int(n / 3) + 1;
-        if (cnt1 >= mini) result.push_back(el1);
-        if (cnt2 >= mini) result.push_back(el2);
+        
+        // now that count is populated, check and push
+        int req = (n / 3) + 1;
+        
+        if(t1 >= req){
+            result.push_back(e1);
+        }
+        if(t2 >= req){
+            result.push_back(e2);
+        }
         return result;
     }
 };
