3c2393b986c8b66ca4f580e3c1fac722aaf65997
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-06-28 19:24:54 +0530

    Time: 26 ms (33.30%), Space: 19.5 MB (95.81%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        
+        // using the moore's voting algorithm - to find the majority element
+        int count = 0, ele = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(count == 0){
+                count++;
+                ele = nums[i];
+            }else if(ele == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        
+        // since it is given for sure that such an element exists
+        return ele;
+    }
+};
