aad7665398660a85f0b6dca5249d2a5ff4dc7eee
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-07-10 14:29:08 +0530

    Time: 47 ms (86.78%), Space: 14.7 MB (19.96%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,66 +9,46 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // using the solution of count
+        // without the use of count
         
-        int c1 = 0, c2 = 0;
+        // since no count, we need to keep reassigning
         
-        // 2 pointers to traverse
-        
-        ListNode* t1 = headA, *t2 = headB;
+        ListNode *t1 = headA, *t2 = headB;
         
         
-        // finding the count
-        
-        while(t1 != NULL){
-            c1++;
-            t1 = t1->next;
-        }
-        
-        
-        while(t2 != NULL){
-            c2++;
-            t2 = t2->next;
-        }
-        
-        
-        // reasssign to head
-        t1 = headA, t2 = headB;
-        
-        int n = abs(c1 - c2);
-        
-        if(c1 < c2){
-            // reduce c2 by n steps
-            while(n != 0){
-                t2 = t2->next;
-                n--;
-            }
-        }
-        
-        else{
-            // reduce c1 by n steps
-            while(n != 0){
+        while(t1 != t2){
+            
+            // they can be null and then should be reassigned
+            
+            
+            if(t1 == NULL) {
+                t1 = headB;
+            }else{
                 t1 = t1->next;
-                n--;
             }
+            
+            
+            if(t2 == NULL){
+                t2 = headA;
+            }else{
+                t2 = t2->next;
+            }
+            
+            
         }
         
+        // here the loop breaks
+        // only when t1 == t2 and they are some node or null
         
-        // now find if there is an intersection
-        
-        while(t1 != NULL && t2 != NULL && t1 != t2){
-            t1 = t1->next;
-            t2 = t2->next;
-        }
+        return t1;
         
         
-        // now the loop breaks
-        if(t1 == t2){
-            return t1;
-        }else{
-            return NULL;
-        }
-        
-
     }
 };
+
+
+
+
+
+
+
