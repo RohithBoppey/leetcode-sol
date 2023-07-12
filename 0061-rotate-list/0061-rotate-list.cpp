2750308a51812b33a40392e9022f54d1dc4d232a
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-07-12 18:38:57 +0530

    Time: 6 ms (76.62%), Space: 11.6 MB (98.20%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,53 +10,52 @@
  */
 class Solution {
 public:
-    
-    // this function gives the length of the linked list
-    
-    int lengthOfLL(ListNode* head){
-        int n = 0;
-        while(head != NULL){
-            n++;
-            head = head->next;
-        }
-        return n;
-    }
-    
-    ListNode* rotate(ListNode* head, int len){
-        ListNode* curr = head, *next;
-        
-        while(len != 2){
-            curr = curr->next;
-            len--;
-        }
-        
-        next = curr->next;
-        
-        // break the link and make the link
-        curr->next = NULL;
-        next->next = head;
-        
-        return next;
-        
-    }
-    
-    
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL || head->next == NULL){
-            // 0 or 1 element
+        
+        if(head == NULL || head == NULL || k == 0){
+            // cannot do anything
             return head;
         }
         
+        // we need to find the count
         
-        // else you need to rotate (n % k) times
+        ListNode* temp = head, *last;
         
-        int n = lengthOfLL(head);
-        int c = k % n;
+        int count = 0;
         
-        while(c != 0){
-            head = rotate(head, n);
-            c--;
+        while(temp != NULL){
+            
+            count++;
+            
+            last = temp;
+            temp = temp->next;
         }
+        
+        // last would be pointing to the last element now
+        
+        last->next = head;
+        
+        // circular has been done
+        
+        // we need to break the find the n % k element and break the link
+        
+        k = k % count;
+        int c = 0, t = count - k - 1;
+        
+        temp = head;
+        
+        while(c != t){
+            c++;
+            temp = temp->next;
+        }
+        
+        // now temp would be pointing to the n - k element
+        
+        // assinging new head
+        head = temp->next;
+        
+        // breaking the bond
+        temp->next = NULL;
         
         return head;
         
