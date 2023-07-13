b62623379ec3b18724fb229aeb8674946d8b83a3
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-07-13 15:27:15 +0530

    Time: 183 ms (67.44%), Space: 23.9 MB (87.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,59 +1,81 @@
 class Solution {
 public:
+    
+    
+    void solve(vector<int>& nums, vector<vector<int>>& result, int n, int target){
+        
+        // this function populates the result array with the ones whose sum is 0
+        
+        int fix; // index that we are fixing
+        int low, high; // index through which we traverse
+        
+        long long int sum;
+        
+        vector<int> ans(3, 0);
+        
+        for(int i = 0; i < n - 2; i++){
+            
+            // ensuring that i is also not repeating
+            if(i != 0 && nums[i] == nums[i - 1]){
+                // not unique
+                continue;
+            }
+            
+            fix = i;
+            low = i + 1;
+            high = n - 1;
+            
+            // finding based on index;
+            // modify low and high
+            
+            while(low < high){
+                sum = nums[low] + nums[fix] + nums[high];
+                if(sum == (long long int) target){
+                    // we found it
+                    ans[0] = nums[low];
+                    ans[1] = nums[fix];
+                    ans[2] = nums[high];
+                    
+                    result.push_back(ans);
+                    ans.resize(3, 0);
+                    
+                    // move to positions which are not repeating
+                    
+                    while(low < high && nums[low] == nums[low + 1])   low++;
+                    while(low < high && nums[high] == nums[high - 1]) high--;
+                    
+                    low++;
+                    high--;
+                }
+                
+                else if(sum < target){
+                    low++;
+                }
+                
+                else{
+                    high--;
+                }
+            }
+            
+        }
+        
+    }
+    
+    
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // using the most optimal approach
-        // fixing one index and using the concept of low and high
+        
+        // we need to sort the array 
+        // for that to be low or high
         
         int n = nums.size();
+        
         vector<vector<int>> result;
-        
-        int low, high, target;
-        
-        vector<int> triplet(3);
         
         sort(begin(nums), end(nums));
         
-        for(int i = 0; i < n - 2; i++){
-            // i is the fixed index
-            
-            // first need to verify whether the i is unique
-            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])){
-                // means that the index i is unique
-                low = i + 1;
-                high = n - 1;
-                
-                // we are trying to find target = - (nums[i]);
-                target = 0 - nums[i];
-                
-                // we need to find target
-                while(low < high){
-                    if(nums[low] + nums[high] == target){
-                        // we have found the index
-                        triplet[0] = nums[low];
-                        triplet[1] = nums[high];
-                        triplet[2] = nums[i];
-                        
-                        result.push_back(triplet);
-                        triplet.resize(3, 0);
-                        
-                        // now that we have found the valid one
-                        // move to the one which is not repeating
-                        
-                        while(low < high && nums[low] == nums[low + 1]) low++;
-                        while(low < high && nums[high] == nums[high - 1]) high--;
-                        low++;
-                        high--;
-                    }
-                    else if(nums[low] + nums[high] < target){
-                        low++;
-                    }else{
-                        high--;
-                    }
-                    
-                }
-            }
-        }
+        solve(nums, result, n, 0);
         
         return result;
+        
     }
 };
