086c08429db74931bab8f086d7c16ea85a292c82
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-07-14 19:25:56 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -6,46 +6,45 @@ using namespace std;
 class Solution
 {
 	public:
-	
-	void dfs(int start, vector<int>& visited, stack<int>& t, vector<int> adj[]){
-	   visited[start] = 1;
-	   for(auto node: adj[start]){
-	       // first push the neighbours and then me
-	       if(visited[node] == 0){
-	           // unvisited
-	           dfs(node, visited, t, adj);
-	       }
-	   }
-	   t.push(start);
-	}
-	
-	
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int n, vector<int> adj[]) 
-	{
-	    // code here
-	    vector<int> ans;
+	{   
+	    // using solution of bfs
 	    
-	    vector<int> visited(n, 0);
-	    
-	    stack<int> t; // to store the neighbours of each
+	    // we need indegrees
+	    vector<int> indegree(n, 0);
 	    
 	    for(int i = 0; i < n; i++){
-	        if(visited[i] == 0){
-	            // unvisited
-	            dfs(i, visited, t, adj);
+	        for(auto x: adj[i]){
+	            indegree[x]++;
 	        }
 	    }
 	    
-	    // pop and add to stack
-	    int top;
-	    int sz = t.size();
+	    queue<int> q;
 	    
-	    while(sz != 0){
-	        top = t.top();
-	        t.pop();
-	        ans.push_back(top);
-	        sz--;
+	    for(int i = 0; i < n; i++){
+	        if(indegree[i] == 0){
+	            // they are the start nodes
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int> ans;
+	    
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        
+	        ans.push_back(node);
+	        
+	        // reduce 1 for all the neighbours
+	        for(auto ne: adj[node]){
+	            indegree[ne]--;
+	            if(indegree[ne] == 0){
+	                // that node is added now
+	                q.push(ne);
+	            }
+	        }
 	    }
 	    
 	    return ans;
