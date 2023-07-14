0fab2b39b60d41a444bd54110da1291d548593b2
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-07-14 19:39:34 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -3,66 +3,58 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    
-    bool solve(int start, vector<int>& visited, vector<int>& in, vector<int> adj[]){
-        
-        // using dfs to find the loop
-        visited[start] = 1;
-        in[start] = 1;
-        
-        for(auto x: adj[start]){
-            // for all the nodes
-            if(visited[x] == 0){
-                // unvisited node
-                if(solve(x, visited, in, adj) == true){
-                    // found a loop in subsequent calls
-                    return true;
-                }
-            }else{
-                // is visited
-                // check if it is visited in the current recursion call
-                if(in[x] == 1){
-                    // visited
-                    // loop exisits
-                    return true;
-                }
-            }
-        }
-        
-        // removing from the current rec call
-        in[start] = 0;
-        return false;
-        
-    }
-    
-    
     bool isCyclic(int n, vector<int> adj[]) {
         // code here
+        // solve using bfs
         
-        // to exist in a directed graph cycle
-        // it has to be in the same recursion stack
-        
-        vector<int> visited(n, 0);
-        vector<int> inRecursion(n, 0);
-        
-        for(int i = 0; i < n; i++){
-            if(visited[i] == 0){
-                // unvisited
-                if(solve(i, visited, inRecursion, adj) == true){
-                    // cycle exisits
-                    return true;
-                }
-            }
-        }
-        
-        // no cycle in all the components 
-        return false;
-        
+        // using the concept of topo sort
+        vector<int> indegree(n, 0);
+	    
+	    for(int i = 0; i < n; i++){
+	        for(auto x: adj[i]){
+	            indegree[x]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    
+	    int countv = 0;
+	    
+	    for(int i = 0; i < n; i++){
+	        if(indegree[i] == 0){
+	            // they are the start nodes
+	            q.push(i);
+	        }
+	    }
+	    
+	    while(!q.empty()){
+	        int node = q.front();
+	        q.pop();
+	        
+	        countv++;
+	        
+	        // reduce 1 for all the neighbours
+	        for(auto ne: adj[node]){
+	            indegree[ne]--;
+	            if(indegree[ne] == 0){
+	                // that node is added now
+	                q.push(ne);
+	            }
+	        }
+	    }
+	    
+	    // check if all the nodes are visited or not
+	    return (countv != n);
+	    
     }
 };
+
+
 
 //{ Driver Code Starts.
 
