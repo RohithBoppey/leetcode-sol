0a8747901db148d2492e0834fb28eb11b347a8ff
RohithBoppey <rohithboppey1298@gmail.com>
2023-07-14 13:21:31 +0530

    Added Directed DFS

---
+++ Detect Cycle in Graph/Directed DFS.cpp
@@ -0,0 +1,60 @@
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    
+    bool solve(int start, vector<int>& visited, vector<int>& in, vector<int> adj[]){
+        
+        // using dfs to find the loop
+        visited[start] = 1;
+        in[start] = 1;
+        
+        for(auto x: adj[start]){
+            // for all the nodes
+            if(visited[x] == 0){
+                // unvisited node
+                if(solve(x, visited, in, adj) == true){
+                    // found a loop in subsequent calls
+                    return true;
+                }
+            }else{
+                // is visited
+                // check if it is visited in the current recursion call
+                if(in[x] == 1){
+                    // visited
+                    // loop exisits
+                    return true;
+                }
+            }
+        }
+        
+        // removing from the current rec call
+        in[start] = 0;
+        return false;
+        
+    }
+    
+    
+    bool isCyclic(int n, vector<int> adj[]) {
+        // code here
+        
+        // to exist in a directed graph cycle
+        // it has to be in the same recursion stack
+        
+        vector<int> visited(n, 0);
+        vector<int> inRecursion(n, 0);
+        
+        for(int i = 0; i < n; i++){
+            if(visited[i] == 0){
+                // unvisited
+                if(solve(i, visited, inRecursion, adj) == true){
+                    // cycle exisits
+                    return true;
+                }
+            }
+        }
+        
+        // no cycle in all the components 
+        return false;
+        
+    }
+};
--- Detect Cycle in Graph/README.md
+++ Detect Cycle in Graph/README.md
@@ -1,1 +1,5 @@
-![image](https://user-images.githubusercontent.com/73538974/253496458-8d649e58-1ab0-453a-80ec-a52fb9572379.png)
+![image](https://user-images.githubusercontent.com/73538974/253508273-1f635719-1c5d-48f0-b44d-ae15d0d894ca.png)
+![image](https://user-images.githubusercontent.com/73538974/253508313-32f64e7f-89cf-4dd7-9dd1-a7b083c4852a.png)
+![image](https://user-images.githubusercontent.com/73538974/253508367-a66cba36-df91-478e-9531-2524a181c62d.png)
+![image](https://user-images.githubusercontent.com/73538974/253508412-f22193cf-5b55-4f7d-914c-0e9b31ae6ebc.png)
+![image](https://user-images.githubusercontent.com/73538974/253508465-ae798e3a-12a9-4661-bd9b-6e2308dbac96.png)
