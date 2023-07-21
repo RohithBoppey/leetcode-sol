3ccda22e0cad886824f98629a93e3600cbfcbb0e
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-07-21 14:33:51 +0530

    Time: 471 ms (45.65%), Space: 10.5 MB (66.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
+        // from bottom to up
+        
         int n = nums.size();
         
-        // using dynamic programming
         vector<int> dp(n, 1);
         
         int mx = 1;
         
-        // the base is 1
-        for(int i = n - 2; i >= 0; i--){
-            for(int j = i + 1; j < n; j++){
-                // compare and find
-                if(nums[i] < nums[j]){
-                    // that means j can be part of subsequence or not
-                    dp[i] = max(dp[i], 1 + dp[j]);   
-                }   
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    // then can be included
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
             }
             mx = max(mx, dp[i]);
         }
         
-        // return the max
         return mx;
-        
         
     }
 };
