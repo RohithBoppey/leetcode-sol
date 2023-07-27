4004808130e1433f1e0948fda5dff6e708911b6d
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-07-27 21:28:32 +0530

    Time: 11 ms (84.71%), Space: 10.8 MB (44.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        
+        // map
         unordered_map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
+        
+        // hash the target - nums[i]
+        
+        for(int i = 0; i < n; i++){
+            
             if(mp.find(target - nums[i]) != mp.end()){
-                return {i, mp[target - nums[i]]};   
+                // found
+                return {i, mp[target - nums[i]]};
             }
+            
             mp[nums[i]] = i;
+            
         }
+        
+        
         return {-1, -1};
     }
 };
