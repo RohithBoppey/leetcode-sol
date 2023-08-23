76766caeb41acbf64177aa450e001878d1fe71f4
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-08-23 16:23:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (21.22%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,48 +1,63 @@
 class Solution {
 public:
+    typedef pair<int, char> P;
+    
     string reorganizeString(string s) {
+        // USING PRIORITY QUEUE SOLUTION
         int n = s.size();
-        vector<int> freq(26, 0);
         
-        // using this to mark max char & its freq 
-        int mxFreq = 0; 
-        char mxChar = 'a';
-        for(int i = 0; i < n; i++){
-            freq[s[i] - 'a']++;
-            if(mxFreq < freq[s[i] - 'a']){
-                mxFreq = freq[s[i] - 'a'];
-                mxChar = s[i];
-            }
-            if(freq[s[i] - 'a'] > (n + 1) / 2){
-                // cannot be possible
+        // populating freq array
+        vector<int> freq(26, 0);
+        for(auto &ch: s){
+            freq[ch - 'a']++;
+            if(freq[ch - 'a'] > (n + 1) / 2){
+                // not possible
                 return "";
             }
         }
         
-        // now frequency array is populated and we know the max occuring element
-        // deal with that first -> fill that first
-        string result = s;
-        int start = 0;
+        // pushing the valid ones into priority queue
+        priority_queue<P, vector<P>> pq;
         
-        while(mxFreq > 0){
-            result[start] = mxChar;
-            start += 2;
-            mxFreq--;
-            freq[mxChar - 'a']--;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0){
+                // that means it exists
+                pq.push({freq[i], (char)('a' + i)});
+            }
         }
         
-        // now fill in the rest
-        for(int i = 0; i < 26; i++){
-            while(freq[i] > 0){
-                // should fill
-                if(start >= n){
-                    // max it will be repeated just once
-                    start = 1;
-                }
-                result[start] = (char)(i + 'a');
-                start += 2;
-                freq[i]--;
+        // processing the pq
+        string result = "";
+        while(pq.size() >= 2){
+            // doing this twice to ensure not repeating
+            P p1 = pq.top();
+            pq.pop();
+            
+            P p2 = pq.top();
+            pq.pop();
+            
+            result += p1.second;
+            result += p2.second;
+            
+            p1.first--;
+            p2.first--;
+            
+            if(p1.first > 0){
+                pq.push(p1);
             }
+            
+            if(p2.first > 0){
+                pq.push(p2);
+            }
+            
+        }
+        
+        // finally if there is some element left in pq, we can safely add
+        if(!pq.empty()){
+            P p1 = pq.top();
+            pq.pop();
+            
+            result += p1.second;
         }
         
         return result;
