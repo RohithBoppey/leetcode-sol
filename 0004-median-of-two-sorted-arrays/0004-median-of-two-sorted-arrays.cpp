960fe792fde1100d9477316e430ae8d1faae8b8a
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-09-21 12:53:00 +0530

    Time: 24 ms (69.42%), Space: 89.8 MB (37.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,60 +1,58 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // using binary search
         
-        // given sorted arrays both
         double ans = 0.0;
         
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-        // better to go with smaller first
-        if(n2 < n1) {
+        // always dealing with min length array
+        if(n2 < n1){
             return findMedianSortedArrays(nums2, nums1);
         }
         
-        int l1,l2,h1,h2,low,high,cut1,cut2;
+        int cut1 = 0, cut2 = 0;
+        int low = 0, high = n1, mid = 0;
         
-        low = 0, high = n1;
+        int l1, l2, r1, r2;
         
-        while(low <= high){
-            cut1 = low + ((high - low) / 2);
-            cut2 = ((n1 + n2 + 1) / 2) - cut1;
+        // find the valid partition
+        while(1){
+            mid = low + ((high - low) / 2);
+            cut1 = mid;
+            cut2 = ((n1 + n2) / 2) - cut1;
             
-            // populating l1, l2, h1, h2;
-            
+            // find l1, l2
             l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
             l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+            r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+            r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
             
-            h1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-            h2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+            if(l1 > r2){
+                // cut1 to the right
+                high = mid - 1;
+            }
             
+            else if(l2 > r1){
+                // cut1 to the left
+                low = mid + 1;
+            }
             
-            if(l1 <= h2 && l2 <= h1){
-                // is valid
-                if((n1 + n2) % 2 == 0){
-                    // even
-                    return (double)(max(l1, l2) + min(h1, h2)) / 2.0;
-                }else{
-                    // odd
-                    // just a single element
-                    return max(l1, l2);
-                }
-            }else{
-                // we need to change the low and high
-                if(l1 > h2){
-                    // we gotta move left
-                    high = cut1 - 1;
-                }else{
-                    low = cut1 + 1;
-                }
-            }            
+            else if(l1 <= r2 && l2 <= r1){
+                // valid partition
+                // cout << l1 << " " << l2 << " " << r1 << " " << r2 << endl;
+                break;
+            }
         }
         
+        
+        ans = ((n1 + n2) % 2 == 0) ? 
+            (double)(max(l1,l2) + min(r1,r2)) / 2 
+            :
+            (double)min(r1,r2);
+        
         return ans;
+        
     }
 };
-
-
-
