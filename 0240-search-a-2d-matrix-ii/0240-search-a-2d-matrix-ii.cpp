4c210f6d0b4b6b1e83c6cbc5c1f51ae62da13da1
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-09-23 16:13:22 +0530

    Time: 96 ms (41.02%), Space: 15.4 MB (8.32%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        // finding the rows and cols
-        
         int rows = matrix.size();
         int cols = matrix[0].size();
         
-        // low and high
-        
         int low = 0, high = cols - 1;
         
-        // new
-        // neither of them can go below 0
         while(low < rows && high >= 0){
             if(matrix[low][high] == target){
-                // found
                 return true;
-            }else if(matrix[low][high] > target){
+            }
+            else if(matrix[low][high] > target){
+                // move left
                 high--;
-            }else{
-                // matrix[low][high] < target
-                // should go down
+            }
+            else{
                 low++;
             }
         }
