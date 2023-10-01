64438694586e0446c73c24d5ff2908890bfe2a5c
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2023-10-01 15:37:26 +0530

    Time: 61 ms (76.37%), Space: 49.5 MB (15.13%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,16 +3,22 @@
  * @return {string}
  */
 
-function reverseBySeparator(string, separator) {
-  return string.split(separator).reverse().join(separator);
+const reverseString = (s) => {
+    let result = "";    
+    let n = s.length;
+    for(let i = n - 1; i >= 0; i--){
+        result += s[i];
+    }
+    return result;
 }
 
-var reverseWords = function(string) {
-//     remember that s is a string
-   // Output becomes !ediuG tpircsavaJ siht ot emocleW
-var reverseEntireSentence = reverseBySeparator(string, "");
-
-// Output becomes emocleW ot siht tpircsavaJ !ediuG
-var reverseEachWord = reverseBySeparator(reverseEntireSentence, " ");
-    return reverseEachWord;
+var reverseWords = function(s) {
+    const words = s.split(" ");
+    let result = "";
+    for(let word of words){
+        result += reverseString(word);
+        result += ' ';
+    }
+    result = result.slice(0, result.length - 1);
+    return result;
 };
