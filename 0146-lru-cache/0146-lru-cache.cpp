4c59fe954c4f0f368b5427190f94e8993880d5f1
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2024-06-17 20:49:57 +0530

    Time: 413 ms (9.03%), Space: 176.1 MB (56.45%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,89 +1,138 @@
 class LRUCache {
 public:
-//     what do we require
-//     we need to implement a doubly linked list and along with their methods
-    class node{
-      public:
-        int key;
-        int value;
-        node* prev;
-        node* next;
-        node(int k, int v){
-            key = k;
-            value = v;
+    
+    class node {
+        public:
+            pair<int,int> val;
+            node* prev;
+            node* next;
+        
+        // initialising each node
+        node(){
+            val = {0,0};
             prev = NULL;
             next = NULL;
         }
+        
+        node(pair<int, int>& p){
+            val = p;
+            prev = NULL;
+            next = NULL;
+        }
+        
     };
+       
+    // required parameters
+    node *head;
+    node *tail;
     
-    node* head = new node(-1, -1);
-    node* tail = new node(-1, -1);
+    unordered_map<int, node*> amp; // amp -> {key, address} mapping
     
-//     additional things we need
-    int cap;
-    unordered_map<int, node*> mp;
+    int cap; // upper limit
     
     LRUCache(int capacity) {
         cap = capacity;
+        // initialise new nodes
+        head = new node();
+        tail = new node();
+        
+        // base case
+        head->prev = NULL;
         head->next = tail;
         tail->prev = head;
+        tail->next = NULL;
     }
     
-    void deleteNode(node* temp){
-        node* prevNode = temp->prev;
-        node* nextNode = temp->next;
-        prevNode->next = nextNode;
-        nextNode->prev = prevNode;
-        // delete(temp);
+    void printDLL(){
+        node* t = head;
+        while(t != NULL){
+            cout << '{' << t->val.first << "," << t->val.second << "}->" ;
+            t = t->next;
+        }
+        cout << endl;
     }
     
-    void addNode(node* temp){
-        node* t = head->next;
-        head->next = temp;
-        temp->prev = head;
-        t->prev = temp;
-        temp->next = t;
+    node* addNodeToBegin(int val, int key){
+        pair<int,int> p = {val, key};
+        node* t = new node(p);
+        node* next = head->next;
+        
+        head->next = t;
+        t->next = next;
+        next->prev = t;
+        t->prev = head;
+        
+        return t;
     }
+    
+    /*
+        check if present in vmp -> {return value, update DLL}
+        yes -> pop from amp -> add to begining -> update amp
+        no -> return -1
+    */
+    
+    void deleteNode(node* d) {
+        node* p = d->prev;
+        node* n = d->next;
+        
+        p->next = n;
+        n->prev = p;
+    }
+    
     
     int get(int key) {
-        if(mp.find(key) != mp.end()){
-            node* temp = mp[key];
-            mp.erase(key);
-            deleteNode(temp);
-            addNode(temp);
-            mp[key] = head->next;
-            return temp->value;
+        // cout << "get " << endl;
+        node* curr;
+        int val;
+        if(amp.find(key) != amp.end()){
+            curr = amp[key];
+            val = curr->val.second;
+            deleteNode(curr);
+            curr = addNodeToBegin(key, val);
+            amp[key] = curr;
+            // printDLL();
+            return val;
         }
         return -1;
     }
     
+    /*
+        check if present in vmp -> {update DLL or add to DLL}
+        yes -> get(key) -> update vmp
+        no -> check size == cap
+            -> yes -> remove end node -> pop end node address -> pop from vmp ->  add to begining -> update amp
+            -> no -> add to begin, done
+    */
+    
     void put(int key, int value) {
-        node* newNode = new node(key, value);
-        if(mp.find(key) != mp.end()){
-//             some value already present at that index
-            node* temp = mp[key];
-            mp.erase(key);
-            deleteNode(temp);
+        node* t;
+        if(amp.find(key) != amp.end()){
+            // exists
+            t = amp[key];
+            t->val.second = value;
+            get(key);
+            return;
         }
-//         now inorder to add it back
-        if(cap == mp.size()){
-//             remove the least recently used element
-//             which is always present at the before tail node
-            node* del = tail->prev;
-            int k = del->key;
-            deleteNode(del);
-            mp.erase(k);
+        // cout << "put  " << endl;
+        // not present, so should add
+        int lv;
+        if(amp.size() == cap){
+            // remove last
+            // cout << "del " << endl;
+            // cout << tail->prev->val.first << " " << tail->prev->val.second << endl;
+            lv = tail->prev->val.first;
+            deleteNode(tail->prev);
+            amp.erase(lv);
         }
-        
-//         now we have free space to insert our element
-        addNode(newNode);
-        mp[key] = head->next;
+        t = addNodeToBegin(key, value);
+        amp[key] = t;
+        // printDLL();
     }
 };
 
 /**
- * Your LFUCache object will be instantiated and called as such:
- * LFUCache* obj = new LFUCache(capacity);
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
