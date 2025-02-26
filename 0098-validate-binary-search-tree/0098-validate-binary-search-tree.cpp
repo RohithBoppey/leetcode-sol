400858d8b1594055804ccdef6db4c0af0f68fbf4
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-02-26 21:33:56 +0530

    Time: 0 ms (100%), Space: 21.8 MB (94.81%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,16 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
 
-    bool solve(TreeNode* root, long l, long h){
+    bool solve(TreeNode* root, long& l, long& h){
 
         if(root == NULL){
             return true;
         }
-
-        cout << root->val << endl;
 
         long val = root->val;
         // check if the selected node is under limits or not
@@ -30,15 +29,20 @@ public:
 
         // the node itself is valid
         // check for left and right
-        
-        if(!solve(root->left, l, val - 1)){
+        int temp = h;
+        h = val - 1;
+
+        if(!solve(root->left, l, h)){
             return false;
         }
+        h = temp;
 
-
-        if(!solve(root->right, val + 1, h)){
+        temp = l;
+        l = val + 1;
+        if(!solve(root->right, l, h)){
             return false;
         }
+        l = temp;
 
         return true;
 
