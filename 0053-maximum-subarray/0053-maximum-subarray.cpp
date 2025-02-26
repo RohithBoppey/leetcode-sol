814ce2ce113ead35d759f6838be1d814d1e54fc9
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-02-26 10:27:39 +0530

    Time: 0 ms (100%), Space: 71.9 MB (18.2%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        // using Kadence's algorithm to find the max subarray sum
         int n = nums.size();
-        int csum = nums[0], msum = nums[0];
+        int csum = nums[0], mx = nums[0];
+        
+        // kadence - the question is: Is it better to start a new subarray from this position or is it better to append to the already formed subarray 
         for(int i = 1; i < n; i++){
-            csum = max(csum + nums[i], nums[i]);
-            msum = max(msum, csum);
+            csum = max(nums[i], csum + nums[i]);
+            mx = max(csum, mx);
         }
-        return msum;
+
+        return mx;
     }
 };
