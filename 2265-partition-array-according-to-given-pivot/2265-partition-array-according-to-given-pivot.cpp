779920666df104ad53e78df797f6f57cd98cf23c
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-03-03 10:25:41 +0530

    Time: 4 ms (80.6%), Space: 139.4 MB (16.39%) - LeetHub

---
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> before, after;
+        int i = 0, n = nums.size(), pcount = 0;
+
+        while(i < n){
+            if(nums[i] < pivot){
+                before.push_back(nums[i]);
+            }
+            else if(nums[i] > pivot){
+                after.push_back(nums[i]);
+            }
+            else{
+                // equal
+                pcount++;
+            }
+            i++;
+        }
+
+        // first before, then pcount, then after
+        vector<int> res;
+        i = 0, n = before.size();
+        while(i < n){
+            res.push_back(before[i]);
+            i++;
+        }
+
+        while(pcount--){
+            res.push_back(pivot);
+        }
+
+        i = 0, n = after.size();
+        while(i < n){
+            res.push_back(after[i]);
+            i++;
+        }
+
+        return res;
+    }
+};
