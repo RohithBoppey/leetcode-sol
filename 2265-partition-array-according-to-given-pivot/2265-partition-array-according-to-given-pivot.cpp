7d82a1ee359c69c2d6fe1954c46f905e3c6c9106
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-03-03 22:19:41 +0530

    Time: 8 ms (50.55%), Space: 127.6 MB (87.5%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> before, after;
-        int i = 0, n = nums.size(), pcount = 0;
+        // using the starting positions to fill in the new array
+        int n = nums.size();
 
+        vector<int> res(n, 0);
+        int lown = 0, equaln = 0;
+        for(auto x: nums){
+            if(x < pivot){
+                lown++;
+            }
+            else if(x == pivot){
+                equaln++;
+            }
+        }
+
+        // start filling from the starting pos
+        int low = 0, equal = lown, high = lown + equaln;
+        int i = 0;
         while(i < n){
             if(nums[i] < pivot){
-                before.push_back(nums[i]);
+                res[low] = nums[i];
+                low++;
             }
             else if(nums[i] > pivot){
-                after.push_back(nums[i]);
+                res[high] = nums[i];
+                high++;
             }
             else{
                 // equal
-                pcount++;
+                res[equal] = nums[i];
+                equal++;
             }
-            i++;
-        }
-
-        // first before, then pcount, then after
-        vector<int> res;
-        i = 0, n = before.size();
-        while(i < n){
-            res.push_back(before[i]);
-            i++;
-        }
-
-        while(pcount--){
-            res.push_back(pivot);
-        }
-
-        i = 0, n = after.size();
-        while(i < n){
-            res.push_back(after[i]);
             i++;
         }
 
         return res;
+    
     }
 };
