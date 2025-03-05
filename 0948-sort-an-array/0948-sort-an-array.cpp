73db0633e04c580bf8900e52cd76da273a714a5a
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-03-05 22:41:24 +0530

    Time: 678 ms (17.43%), Space: 295.5 MB (17.01%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int n;
+
+    void mergeBoth(vector<int>& v, int low, int high, int mid){
+        // take both the array and put in back in the array
+        vector<int> temp;
+
+        int lp = low, rp = mid + 1;
+
+        // the ranges for the pointers are [low, mid] & [mid + 1, high]
+        while(lp <= mid && rp <= high){
+            // compare and proceed
+            if(v[lp] < v[rp]){
+                // move left
+                temp.push_back(v[lp]);
+                lp++;
+            }else{
+                // move right
+                temp.push_back(v[rp]);
+                rp++;
+            }
+        }
+
+        // copy the remaining elements
+        while(lp <= mid){
+            temp.push_back(v[lp]);
+            lp++;
+        }
+
+        while(rp <= high){
+            temp.push_back(v[rp]);
+            rp++;
+        }
+
+        // now both are copied, come back to the previous array
+        for(int i = low; i <= high; i++){
+            v[i] = temp[i - low];
+        }
+
+    }
+
+    void sortRec(vector<int>& v, int low, int high){
+        
+        if(low >= high){
+            // single elment or no elements
+            // nothing to sort
+            return;
+        }
+
+
+        int mid;
+
+        mid = low + (high - low) / 2;
+
+        // sort the left side of the array
+        sortRec(v, low, mid);
+
+        // sort the right part of the array
+        sortRec(v, mid + 1, high);
+
+        // merge both the parts now
+        mergeBoth(v, low, high, mid);
+        
+    
+    }
+
+
+    vector<int> sortArray(vector<int>& nums) {
+        // using merge sort to solve the problem
+        n =  nums.size();
+        sortRec(nums, 0, n - 1);
+        return nums;
+
+    }
+};
