65959a60a3a8a41049cb7577767c9ba634c53d64
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-03-19 22:43:56 +0530

    Time: 14 ms (19.6%), Space: 146.9 MB (5.33%) - LeetHub

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+
+        // add another array for the count of flips for each
+        vector<int> flips(n, 0);
+        int tf = 0; //total flips
+
+        int i = 0;
+        while(i < n){
+            // check and do the flip
+            if(
+                (nums[i] == 0 && flips[i] % 2 == 1) 
+                || 
+                (nums[i] == 1 && flips[i] % 2 == 0)
+            ){
+                // no need of flipping
+                i++;
+            }else{
+                // we need to flip the k-size window
+                int t = k, j = i;
+
+                // boundary condition
+                if(i > n - k){
+                    // cannot proceed further
+                    return -1;
+                }
+
+                tf++;
+                while(t != 0){
+                    flips[j]++;
+                    j++;
+                    t--;
+                }
+                i++;
+            }
+        }
+
+        return tf;
+
+    }
+
+    int minOperations(vector<int>& nums) {
+        return minKBitFlips(nums, 3);
+    }
+};
