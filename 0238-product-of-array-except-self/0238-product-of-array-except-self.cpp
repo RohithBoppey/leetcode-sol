ef4a6d5a06bea20295b84c535d235cf23434d7c6
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-05-27 09:48:31 +0530

    Time: 0 ms (100%), Space: 40.2 MB (58.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
+    int n;
+
     vector<int> productExceptSelf(vector<int>& nums) {
-        int product = 1, np = 1, allZero = 1, nz = 0;
-        int n = nums.size();
-        for(auto x: nums){
-            product *= x;
-            if(x != 0){
-                np *= x;
-                allZero = 0;
-            }else{
-                nz++;
-            }
-        }
-        
-        if(allZero == 1){
-            // all are zeroes
-            return vector<int>(n, 0);
-        }
-        
-        vector<int> res(n, 1);
+        // final optimized solution
+
+        n = nums.size();
+
+        //  calculate prefix and suffix on the go
+        vector<int> ans(n, 1);
+
+        int curr;
+        // prefix
+        curr = 1;
+        // left sum
         for(int i = 0; i < n; i++){
-            if(nums[i] == 0){
-                if(nz == 1){
-                    // only this is the zero
-                    res[i] = np;
-                }else{
-                    res[i] = 0;
-                }
-            }else{
-                res[i] = product / nums[i];
-            }
+            // update the current value with the prefix calculated so far
+            ans[i] *= curr;
+            // update current so it includes the current value --> need it for the next step
+            curr *= nums[i];
         }
-        return res;
+
+        // right sum
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--){
+            // take in the current value with suffix 
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+
+        return ans;
+
     }
 };
