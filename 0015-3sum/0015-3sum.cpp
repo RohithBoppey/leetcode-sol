59425a280bbb2c6a9e130b554f68c206ba44178d
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-05-29 10:33:42 +0530

    Time: 60 ms (29.81%), Space: 32.5 MB (24.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,81 +1,73 @@
 class Solution {
 public:
-    
-    
-    void solve(vector<int>& nums, vector<vector<int>>& result, int n, int target){
-        
-        // this function populates the result array with the ones whose sum is 0
-        
-        int fix; // index that we are fixing
-        int low, high; // index through which we traverse
-        
-        long long int sum;
-        
+
+    vector<vector<int>> res;
+
+    // generic way
+    void solve(vector<int>& nums, int target){
+        int n = nums.size(); 
+
+        int low, high;
+        int sum = target; 
+
         vector<int> ans(3, 0);
+        sort(nums.begin(), nums.end());
         
-        for(int i = 0; i < n - 2; i++){
-            
-            // ensuring that i is also not repeating
+        // need 3 elements, so iterating only until n - 2
+        for(int i = 0; i < n - 2; i++){ 
+
+            // to remove duplicate processing
             if(i != 0 && nums[i] == nums[i - 1]){
-                // not unique
                 continue;
             }
-            
-            fix = i;
+
+            sum = 0;
+            // starting from immediate pos
             low = i + 1;
             high = n - 1;
-            
-            // finding based on index;
-            // modify low and high
-            
-            while(low < high){
-                sum = nums[low] + nums[fix] + nums[high];
-                if(sum == (long long int) target){
-                    // we found it
-                    ans[0] = nums[low];
-                    ans[1] = nums[fix];
-                    ans[2] = nums[high];
-                    
-                    result.push_back(ans);
-                    ans.resize(3, 0);
-                    
-                    // move to positions which are not repeating
-                    
-                    while(low < high && nums[low] == nums[low + 1])   low++;
+
+            // keep finding the sum by adjusting low and high accordingly
+            while(low < high){ // not coincide coz we need 3 ele   
+                // check the sum now
+
+                sum = nums[low] + nums[high] + nums[i];
+
+                if(sum == target){
+                    // put in ans
+                    ans = {nums[i], nums[low], nums[high]};
+                    res.push_back(ans);
+
+                    // move to the next low and high to prevent duplicates
+                    while(low < high && nums[low] == nums[low + 1]) low++;
                     while(low < high && nums[high] == nums[high - 1]) high--;
-                    
+
+                    // now it is pointing to the repeated value last occurance
                     low++;
                     high--;
+
+                }else{
+                    // adjust low and high
+                    if(sum < target){
+                        // sum is left, can increase low
+                        low++;
+                    }else{
+                        // sum is negative, should decrease high
+                        high--;
+                    }
                 }
-                
-                else if(sum < target){
-                    low++;
-                }
-                
-                else{
-                    high--;
-                }
+
+
             }
-            
+
         }
-        
+
     }
-    
-    
+
     vector<vector<int>> threeSum(vector<int>& nums) {
         
-        // we need to sort the array 
-        // for that to be low or high
-        
-        int n = nums.size();
-        
-        vector<vector<int>> result;
-        
-        sort(begin(nums), end(nums));
-        
-        solve(nums, result, n, 0);
-        
-        return result;
-        
+        int n = nums.size(); 
+        int target_sum = 0; /// can change whenever needed
+        solve(nums, target_sum);
+        return res;
     }
 };
