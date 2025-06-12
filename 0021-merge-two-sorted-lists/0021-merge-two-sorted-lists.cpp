7329871514c0de8bc09514637b210b7fd09bd497
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-12 20:38:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.91%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,40 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* h1, ListNode* h2) {
-        if(h1 == NULL){
-            return h2;
-        }
-        
-        if(h2 == NULL){
-            return h1;
-        }
-        
-        
-        // l1 is always low, l2 is always high
-        if(h1->val > h2->val){
-            return mergeTwoLists(h2, h1);
-        }
-        
-        
-        ListNode *l1 = h1, *l2 = h2, *result = h1;
-        ListNode* temp = NULL;
-        
-        while(l1 != NULL && l2 != NULL){
-            temp = l1;
-            
-            while(l1 != NULL && l1->val <= l2->val){
-                // keep moving l1 and update temp
-                temp = l1;
-                l1 = l1->next;
+    ListNode* merge(ListNode* l1h, ListNode* l2h){
+
+        ListNode* prev = new ListNode(0), *head = l1h, *next = NULL;
+        prev->next = l1h;
+
+        while(l1h != NULL && l2h != NULL){
+            if(l1h->val > l2h->val){
+                // pull the node from l2 and put in l1
+                next = l2h->next;
+                prev->next = l2h;
+                l2h->next = l1h;
+                l2h = next;
+            }else{
+                l1h = l1h->next;
             }
-            
-            temp->next = l2;
-            swap(l1, l2);
+            prev = prev->next;
         }
-        
-        
-        return result;
-        
+
+        // if l2 is remaining with any elements
+        if(l2h != NULL){
+            prev->next = l2h;
+        }
+
+        return head;
+    }
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL && l2 == NULL){
+            return NULL;
+        }
+        if(l1 == NULL){
+            return l2;
+        }
+        if(l2 == NULL){
+            return l1;
+        }
+        // choose the smaller one
+        return (l1->val < l2->val) ? merge(l1, l2) : merge(l2, l1); 
     }
 };
