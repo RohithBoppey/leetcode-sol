f09fd6a897020a1e83934f49dc56fbc771e23b52
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-12 09:37:31 +0530

    Time: 0 ms (100%), Space: 13.7 MB (5.81%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& v, int target) {
-        if(target <= v[0]){
+    int searchInsert(vector<int>& nums, int target) {
+        // handle the edge case seperately
+        int n = nums.size(); 
+
+        if(target < nums[0]){
+            // at start
             return 0;
-        }
-        int n = v.size();
-        if(target > v[n-1]){
+        } 
+        if(target > nums[n - 1]){
+            // at the end
             return n;
         }
-        int low = 0, high = n - 1, mid = 0;
+
+        // binary search code
+        int low = 0, high = n - 1, mid;
+
         while(low <= high){
             mid = low + (high - low) / 2;
-            if(v[mid] == target){
-                return mid;
-            }else if(v[mid] < target){
-                low = mid + 1;
-            }else{
+
+            if(nums[mid] == target){
+                return mid; // this is the index
+            }
+            else if(nums[mid] > target){
+                // reduce the mid search space
                 high = mid - 1;
+            }else{
+                // < target
+                // should increase
+                low = mid + 1;
             }
         }
-        
+
+        // if reached here, meaning low == high
         return low;
     }
 };
