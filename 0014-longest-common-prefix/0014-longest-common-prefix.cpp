57d3dcec863ea1fa072cd8239621ea9abf71e999
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-12 18:35:32 +0530

    Time: 0 ms (100%), Space: 11.8 MB (84.5%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,52 +1,19 @@
 class Solution {
 public:
-    bool allMatched(vector<string>& strs, int mid){
-        // for all the strings, check from start to mid characters
-        for(int i = 1; i < strs.size(); i++){
-            for(int j = 0; j < mid; j++){
-                // checking till j characters
-                if(strs[i][j] != strs[0][j]){
-                    return false;
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = "";
+
+        // first loop: letter, then: loop
+        for(int i = 0; i <= strs[0].size(); i++){
+            for(int j = 0; j < strs.size(); j++){
+                // comparing it with the first word only
+                if(i == strs[j].size() || strs[j][i] != strs[0][i]){
+                    return res;
                 }
             }
+            res += strs[0][i];
         }
-        
-        return true;
-    }
-    
-    
-    string longestCommonPrefix(vector<string>& strs) {
-        // this solution uses binary search
-        
-        // we are using the binary search to find the max length upto which the prefix is same
-        
-        int low = 0, high = INT_MAX, sz;
-        for(auto &s: strs){
-            sz = s.length();
-            high = min(high, sz);
-            // checking min should be enough
-        }
-        
-        int mid;
-        
-        int ans = 0;
-        
-        // using binary search from low to high
-        while(low <= high){
-            mid = low + (high - low) / 2;
-            if(allMatched(strs, mid) == true){
-                // all the characters matched
-                ans = mid;
-                low = mid + 1;
-            }else{
-                // did not match
-                // so converse
-                high = mid - 1;
-            }
-        }
-        
-        
-        return strs[0].substr(0, ans);
-        
+
+        return res;
     }
 };
