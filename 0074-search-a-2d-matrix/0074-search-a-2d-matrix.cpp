c82e9f4fe61b0ee8006af16e2921d75b044baa5a
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-12 12:54:27 +0530

    Time: 0 ms (100%), Space: 13.3 MB (42.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        
-        // we have indexes from [0, n - 1]
-        int left = 0, right = (rows*cols) - 1, mid;
+    int rows, cols;
+    pair<int,int> findInd(int &ind){
         int row, col;
-        
-        while(left <= right){
-            mid = left + (right - left) / 2;
-            row = mid / cols;
-            col = mid % cols;
-            
-            if(matrix[row][col] == target){
-                // found
+        row = ind / cols;
+        col = ind - (row * cols);
+        return {row, col};
+    }
+
+    bool searchMatrix(vector<vector<int>>& m, int target) {
+        rows = m.size();
+        cols = m[0].size(); 
+
+        //  the possible indices are: 0 to rows * cols - 1
+        int low = 0, high = (rows * cols) - 1, mid;
+        int row, col, val;
+        pair<int,int> p;
+
+        while(low <= high){
+            mid = low + (high - low) / 2;
+
+            p = findInd(mid);
+            row = p.first;
+            col = p.second;
+            val = m[row][col];
+
+            // cout << val << endl;
+
+            if(val == target){
                 return true;
             }
-            
-            else if(matrix[row][col] < target){
-                // modify left
-                left = mid + 1;
+            else if(val > target){
+                // decrease index
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
-            
-            else{
-                right = mid - 1;
-            }
-            
+
         }
-        
+
+        // not found
         return false;
-        
+
     }
 };
