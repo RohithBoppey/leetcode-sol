a2e3a0e2ba075de25270201477b7bd5b301051d5
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-12 18:50:56 +0530

    Time: 0 ms (100%), Space: 9.8 MB (27.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,36 @@
 class Solution {
 public:
+    void printV(vector<int>& v){
+        for(auto &x: v){
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        return (s == t);
+        // taking count
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        int l = s.size();
+        vector<int> s1(26, 0), s2(26, 0);
+
+        for(int i = 0; i < l; i++){
+            s1[s[i] - 'a']++;
+            s2[t[i] - 'a']++;
+        }
+
+        // printV(s1);
+        // printV(s2);
+
+        for(int i = 0; i < 26; i++){
+            if(s1[i] != s2[i]){
+                return false;
+            }
+        }
+
+
+        return true;
     }
 };
