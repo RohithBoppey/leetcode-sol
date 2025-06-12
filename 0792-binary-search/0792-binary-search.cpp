6863c1eab35705a7e69642866acc2e6f7d2f01e6
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-12 09:12:30 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.51%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // using binary search
+        int n = nums.size(); 
+
+        int low = 0, high = n - 1, mid;
+
+        while(low < high){
+            mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                // should increase
+                low = mid + 1;
+            }else{
+                // > target
+                high = mid - 1;
+            }
+        }
+
+        // if both coincide, meaning exist
+        return (nums[low] == target) ? low : -1;
+    }
+};
