3d3eeb547d992b6bd68d3da61ddf15121dcffe5f
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-16 13:18:32 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.91%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    int uniquePaths(int rows, int cols) {
-        
-        // initialising since everything is possible
-        vector<vector<int>> dp(rows, vector<int>(cols, 1));
-        
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < cols; j++){
-                // processing based on both the ways possible
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    void print2d(vector<vector<int>>& dp){
+        for(auto &x: dp){
+            for(auto &y: x){
+                cout << y << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    int uniquePaths(int m, int n) {
+        // using memoization way
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // initialization
+        for(int i = 0; i < n; i++){
+            dp[0][i] = 1;
+        }
+
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+
+        // processing
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
-        
-        return dp[rows-1][cols-1];
+
+        // print2d(dp);
+
+        return dp[m - 1][n - 1];
     }
 };
