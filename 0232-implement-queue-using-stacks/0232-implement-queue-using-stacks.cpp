07db711335cf6b7ab37f4b63d0e31ed9d3f8a23b
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-16 18:10:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (26.56%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,82 +1,58 @@
 class MyQueue {
 public:
-    
-    stack<int> s1;
-    stack<int> s2;
-    
+    stack<int> s1, s2;
+
     MyQueue() {
         
     }
     
     void push(int x) {
-        // push into non-empty ones
-        // if(!s2.empty()){
-        //     s2.push(x);
-        // }else{
-        //     // either s1 can be empty, or non-empty, so win-win!
-        //     s1.push(x);
-        // }
-        
+        // push all into the stack s1
+        // s2 acts as helper stack
         s1.push(x);
     }
     
-    // move all from s1 to s2 until just one element remains in the stack
-    // again move/pop based on method
-    // push all those back to s1 
-    // return the first
-    
-    int move(stack<int>& s1, stack<int>& s2, int method){
-        int first;
-        while(s1.size() != 1){
-            first = s1.top();
+    // if method == 0 -> top, if 1 -> then pop
+    int transfer(int method = 0){   
+        // always move from s1 to s2
+        int top = -1;
+        while(s1.size() > 1){
+            top = s1.top();
             s1.pop();
-            s2.push(first);
+            s2.push(top);
         }
-        
-        // just one element remains
-        first = s1.top();
+
+        top = s1.top();
         s1.pop();
-        
-        if(method == 0){
-            // just top, so we want this element
-            s2.push(first);
+        if(method == 1){
+            // pop: remove that element as well
+        }else{
+            // top: can push back
+            s2.push(top);
         }
-        
-        // push all those elements back into the s1
-        int t;
+
+        // helper method done
+        // so push back into main stack
         while(!s2.empty()){
-            t = s2.top();
+            int t = s2.top();
             s2.pop();
             s1.push(t);
         }
         
-        return first;
+        return top;
+
     }
-    
+
     int pop() {
-        int first;
-        // if(!s1.empty()){
-        //     first = move(s1, s2, 1);
-        // }else{
-        //     first = move(s2, s1, 1);
-        // }
-        first = move(s1, s2, 1);
-        return first;
+        return transfer(1);
     }
     
     int peek() {
-        int first ;
-        // if(!s1.empty()){
-        //     first = move(s1, s2, 0);
-        // }else{
-        //     first = move(s2, s1, 0);
-        // }
-        first = move(s1, s2, 0);
-        return first;
+        return transfer();
     }
     
     bool empty() {
-        return s1.empty() && s2.empty();
+        return s1.empty();
     }
 };
 
