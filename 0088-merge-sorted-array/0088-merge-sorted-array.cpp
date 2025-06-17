8d81bf709be1410328d89b38c8d9808c08ce1814
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-17 11:01:12 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.55%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
-    void swap1(vector<int>& a, int index1, vector<int>& b, int index2){
-        if(a[index1] > b[index2]){
-            swap(a[index1], b[index2]);
-        }
-    }
-    
-    void merge(vector<int>& a, int m, vector<int>& b, int n) {
-        // Write your code here.
-        // best optimal one - using GAP OR shell sort algorithm
-        int len = n + m;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // using pointers for tracking positions and insertion
+        int seen = m - 1; // last known pointer in the nums 1
+        int r = n - 1; // last max value in the nums 2
+        int to = m + n - 1; // which posititon to insert starting from the end
 
-        // Initial gap:
-        int gap = (len / 2) + (len % 2);
-
-        while (gap > 0) {
-            // Place 2 pointers:
-            int left = 0;
-            int right = left + gap;
-            while (right < len) {
-                // case 1: left in arr1[]
-                //and right in arr2[]:
-                if (left < m && right >= m) {
-                    swap1(a, left, b, right - m);
-                }
-                // case 2: both pointers in arr2[]:
-                else if (left >= m) {
-                    swap1(b, left - m, b, right - m);
-                }
-                // case 3: both pointers in arr1[]:
-                else {
-                    swap1(a, left, a, right);
-                }
-                left++, right++;
+        // this loop can run until either of the pointer is null
+        // if r pointer is null, all elements are inserted into the main array
+        // if r is not null, that means there are some elements in the nums2 which are less than the least element in nums1 i.e. should be inserted at the start
+        while(seen >= 0 && r >= 0){
+            if(nums1[seen] < nums2[r]){
+                // should be inserted at the end
+                nums1[to] = nums2[r];
+                r--;
+            }else{
+                // nums1[seen] >= nums2[r]
+                // fill the value in the nums1 itself
+                nums1[to] = nums1[seen];
+                seen--; 
             }
-            // break if iteration gap=1 is completed:
-            if (gap == 1) break;
-
-            // Otherwise, calculate new gap:
-            gap = (gap / 2) + (gap % 2);
+            to--;
         }
-        
-        // nums2 is sorted
-        // hence we need to copy
-        for(int i = 0; i < n; i++){
-            a[m + i] = b[i];
+
+        // now there might be remaining elements in the r
+        while(r >= 0){
+            // fill the nums1 with to
+            nums1[to] = nums2[r];
+            r--;
+            to--;
         }
     }
 };
