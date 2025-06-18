2965233d37c27de3b3c27953707675ea6705f924
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-18 21:32:30 +0530

    Time: 4 ms (48.54%), Space: 36.5 MB (41.81%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -14,37 +14,65 @@ public:
         }
     }
 
+    int n;
+    // binary search to give the closest
+    int closest(vector<int>& arr, int x){
+        int low = 0, high = n - 1, mid;
+
+        // boundary condition
+        if(arr[0] >= x){
+            return 0;
+        }
+
+        if(arr[n - 1] <= x){
+            return n - 1;
+        }
+ 
+        while(low < high){
+            mid = low + (high - low) / 2;
+            // cout << low << " " << high << " " << mid << endl;
+            if(arr[mid] == x){
+                return mid;
+            }
+            if(arr[mid] > x){
+                // reduce space
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+
+        // meaning high = low
+        return low; 
+    }
+
+
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         vector<int> ans;
+        n = arr.size(); 
+        // using binary search to find the index
+        int index = closest(arr, x);
+        
+        // cout << index << endl; 
+        cout << lower_bound(begin(arr), end(arr), x) - begin(arr) << endl;
+        
+        // now using the 2 pointers to find out the least k values
+        int left = index - 1, right = index; 
+        int lv, rv; 
 
-        // building a vector based on differences
-        // for getting the max diff, I need the min element
-        int mx = INT_MIN;
+        while(k > 0){
+            lv = (left >= 0) ? x - arr[left] : INT_MAX;
+            rv = (right <= n - 1) ? arr[right] - x : INT_MAX;
 
-        for(auto &y: arr){
-            mx = max(abs(x - y), mx);
-        }
-
-        // cout << mx << endl;
-
-        vector<vector<int>> diff(mx + 1);
-
-        for(auto &t: arr){
-            int d = abs(x - t);
-            diff[d].push_back(t);
-        }
-
-        // print2d(diff);
-
-        for(int i = 0; i <= mx; i++){
-            for(auto& x: diff[i]){
-                if(k < 1){
-                    // top k found
-                    break;
-                }
-                ans.push_back(x);
-                k--;
+            if(lv <= rv){
+                // go left after including this
+                ans.push_back(arr[left]);
+                left--;
+            }else{ 
+                ans.push_back(arr[right]);
+                right++;
             }
+            k--;
         }
 
         // sort the result
