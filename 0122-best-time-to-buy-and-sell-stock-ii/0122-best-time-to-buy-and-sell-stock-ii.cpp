4e2310dc56e3b809b8ce03ed47c7bd914fc35a9d
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-06-26 09:37:36 +0530

    Time: 0 ms (100%), Space: 17.2 MB (67.79%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,46 +1,14 @@
 class Solution {
 public:
-    
-    int dp[30001][2];
-    
-    int solve(vector<int>& prices, int day, bool buy, int &n){
-        // this function will return max profit that can be obtained starting from day 
-        if(day >= n){
-            // cannot generate
-            return 0;
-        }
-        
-        if(dp[day][buy] != -1){
-            // already cal
-            return dp[day][buy];
-        }
-        
-        // need to cal
-        
-        int profit = 0;
-        
-        if(buy == true){
-            // he can either buy or not
-            // if buying, he is spending money, so profit - currentAmount
-            int buy = solve(prices, day + 1, false, n) - prices[day];
-            int notbuy = solve(prices, day + 1, true, n);
-            profit = max({profit, buy, notbuy});
-            
-        }else{
-            // he can sell or nto
-            // if selling, he gets the money + next profit
-            int sell = solve(prices, day + 1, true, n) + prices[day];
-            int notsell = solve(prices, day + 1, false, n);
-            profit = max({sell, notsell, profit});
-        }
-        
-        return dp[day][buy] = profit;
-        
-    }
-    
     int maxProfit(vector<int>& prices) {
+        // greedy would work here
         int n = prices.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(prices, 0, true, n);
+        int profit = 0;
+
+        for(int i = 1; i < n; i++){
+            profit += (prices[i] > prices[i - 1]) ? prices[i] - prices[i - 1] : 0;
+        }
+
+        return profit; 
     }
 };
