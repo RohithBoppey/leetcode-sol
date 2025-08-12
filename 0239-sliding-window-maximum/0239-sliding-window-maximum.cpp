d7288f263ab71aaaeead633c4822f1f77fb61265
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-08-13 00:24:51 +0530

    Time: 71 ms (14.8%), Space: 156.3 MB (11.01%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    
     priority_queue<pair<int,int>> pq;
+    vector<int> ans; 
     int n;
-    
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // using priority queue solution
-        n = nums.size();
+        n = nums.size(); 
         int i = 0, j = 0;
-        int c, v;
-        vector<int> ans;
+
         while(j < n){
-            c = nums[j];
-            pq.push({c, j});
-            // the current index -> top should be in the range [j, j - k + 1]
-            if(j - i + 1 < k){
-                // nothing to do
+            // no matter what, push in the queueu
+            pq.push({nums[j], j});
+
+            if(j - i + 1  < k){
+                // do nothing, as the window size is not met
             }else{
+                // window size met
+                // now until you find the max value which is under the max limit that we can go back
+                // i.e for an index j, we can go back k elements
+                // i.e the top should lie in [j - k + 1, j]
                 while(!pq.empty() && pq.top().second < j - k + 1){
+                    // removing the outdated elements
                     pq.pop();
                 }
-                v = pq.top().first;
-                ans.push_back(v);
+
+                // now the element found: inside the limits and the first max in the array
+                // add inside the array
+                ans.push_back(pq.top().first);
+
+                // since window size is met, keep incrementing i
                 i++;
-           }
+            }
             j++;
         }
-        return ans;
+
+        return ans; 
     }
 };
