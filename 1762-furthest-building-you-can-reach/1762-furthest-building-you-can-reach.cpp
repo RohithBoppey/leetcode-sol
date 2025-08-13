d408c89c075cbb91e3f10475417bc928d905381d
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-08-13 12:10:29 +0530

    Time: 16 ms (26.74%), Space: 58.4 MB (22.08%) - LeetHub

---
+++ 1762-furthest-building-you-can-reach/1762-furthest-building-you-can-reach.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    priority_queue<int> pq;
+    int n; 
+    int solve(vector<int>& v, int b, int l){
+        n = v.size(); 
+
+        int start = 0;
+
+        while(start < n - 1){
+            // cout << v[start] << endl;
+            if(v[start] >= v[start + 1]){
+                // can jump directly
+                start++;
+            }else{
+                // you will need a ladder or bricks
+                int diff = v[start + 1] - v[start];
+
+                // first use bricks
+                b -= diff; 
+                pq.push(abs(diff));
+
+                if(b < 0 and l > 0){
+                    // maybe we can use a ladder near the previously max height seen so far
+                    int top = pq.top();
+                    pq.pop(); 
+                    b += top; 
+                    l--; 
+                }
+
+                if(b < 0 and l <= 0){
+                    // no way to proceed
+                    return start; 
+                }
+                start++;
+            }
+        }
+
+        return start; 
+
+    }
+
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        return solve(heights, bricks, ladders);
+    }
+};
