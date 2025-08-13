5aa0ddb4ead5c5f3b1c0952a52ea0e6822eaf873
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-08-13 13:01:16 +0530

    Time: 43 ms (17.43%), Space: 31.9 MB (14.4%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    unordered_map<string, vector<string>> mp; 
     vector<vector<string>> ans; 
+    unordered_map<string, vector<string>> mp; 
 
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size(); 
-        
-        for(auto &x: strs){
-            string y = x; 
-            sort(y.begin(), y.end());
-            // cout << y << endl; 
-            mp[y].push_back(x);
-        }
+        string s; 
+        for(auto &str: strs){
+            int n = str.size(); 
+            vector<int> curr(26, 0); 
+            // populate count
+            for(auto &ch: str){
+                curr[ch - 'a']++;
+            }
+            // make the string
+            s = "";
+            for(int i = 0; i < 26; i++){
+                if(curr[i] != 0){
+                    s += to_string(i) + to_string(curr[i]) + ',';
+                }
+            }
+            // cout << s << endl;
+            // put the key in map 
+            mp[s].push_back(str);
+        }        
 
+        // build final map
         for(auto &it: mp){
             ans.push_back(it.second);
         }
 
-        return ans; 
-
+        return ans;
     }
 };
