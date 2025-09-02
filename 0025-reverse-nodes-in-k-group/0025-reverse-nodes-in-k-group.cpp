af183443e4ff03f376e0b3840c55031d5a70868d
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-09-02 11:35:12 +0530

    Time: 0 ms (100%), Space: 16.6 MB (38.97%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,73 +10,47 @@
  */
 class Solution {
 public:
-    
-    int findLen(ListNode* head){
-        ListNode* temp = head;
+    int findCount(ListNode* head){
         int count = 0;
-        
-        while(temp != NULL){
-            temp = temp->next;
+        while(head != NULL){
             count++;
+            head = head->next;
         }
-        
         return count;
     }
-    
-    
+
     ListNode* solve(ListNode* head, int left, int k){
-        
-        // this function return the reversed by k group linked list
-        
+        // how much is left, how much to rotate
         if(left < k){
-            // cannot reverse, left elements are less than k
-            return head;
+            // should not rotate
+            return head; 
         }
-        
-        // rotate the k group from the head position
-        
-        ListNode* curr = head, *next, *prev = NULL;
-        
-        int count = 0;
-        
-        while(curr != NULL && count < k){
-            // since we are required to only rotate k elements
-            
-            // 4 operations to rotate
-            next = curr->next;
-            curr->next = prev;
+
+        ListNode* curr = head, *next, *prev = NULL; 
+        int c = 0; 
+
+        while(curr != NULL && c < k){
+            next = curr->next; 
+            curr->next = prev; 
             prev = curr;
-            curr = next;
-            
-            // to manage
-            count++;
+            curr = next; 
+
             left--;
+            c++;
         }
-        
-        
-        // now the k group has been rotated
-        // the new list contains the head to be the last element pointing to NULL
-        
-        // that we are updating from the later on recursive call
-        
+
+        // now the first k nodes are reversed, join this with the remaining part
+        // head is the first node
         if(next != NULL){
             head->next = solve(next, left, k);
         }
-        
-        return prev;
-        
+
+        return prev; 
     }
-    
+
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head == NULL || head->next == NULL || k == 0){
-            // 0 or 1 elements
-            // nothing to rotate
-            return head;
-        }
-        
-        int count = findLen(head); // this function gives the len of the linked list
-        
-        return solve(head, count, k);
-        
+        int count = findCount(head);
+        ListNode* h = solve(head, count, k);
+        return h;   
     }
 };
