de8ddccf88ae4c0827ab042aa376621312158961
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-09-11 21:32:41 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
-        int n = nums.size();
+        int n;
+        n = nums.size(); 
+        vector<int> dp(n + 1, 0); 
+
         if(n == 1){
             return nums[0];
         }
 
-        // idea: to store max amount that can be robbed starting from the house i
-        vector<int> r(n, 0);
-        r[n - 1] = nums[n - 1]; // can only rob this 1 store
-        r[n - 2] = max(r[n - 1], nums[n - 2]);
-
-        int c = r[n - 2]; // running pointer to store max
+        dp[n - 1] = nums[n - 1];
+        dp[n - 2] = max(nums[n - 1], nums[n - 2]);
 
         if(n == 2){
-            return c;
+            return dp[n - 2];
         }
 
-        int rb, nrb;
         for(int i = n - 3; i >= 0; i--){
-            // at any point, i can rob or not rob
-            rb = nums[i] + r[i + 2];
-            nrb = c;
+            int rb, nrb; 
+            rb = nums[i] + dp[i + 2];
+            nrb = dp[i + 1];
 
-            c = max(rb, nrb);
-
-            r[i] = c;
+            dp[i] = max(rb, nrb);
         }
 
-        return c;
-
+        return dp[0];
     }
 };
