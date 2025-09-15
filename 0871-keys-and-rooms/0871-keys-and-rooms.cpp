9e61e0b9dd53a2eba14eabce951e6b6858793dc0
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-09-15 11:32:46 +0530

    Time: 0 ms (100%), Space: 14.8 MB (33.1%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<int> vis(n, 0);
+        vis[0] = 1; // default
+
+        queue<int> q;
+        q.push(0); 
+
+        while(!q.empty()){
+            int t = q.front();
+            q.pop(); 
+
+            for(auto &neigh: rooms[t]){
+                if(vis[neigh] == 0){
+                    vis[neigh] = 1;
+                    q.push(neigh);
+                }
+            }
+        } 
+
+        for(int i = 0; i < n; i++){
+            // cout << vis[i] << " ";
+            if(vis[i] == 0) return false; 
+        }
+
+        return true; 
+
+    }
+};
