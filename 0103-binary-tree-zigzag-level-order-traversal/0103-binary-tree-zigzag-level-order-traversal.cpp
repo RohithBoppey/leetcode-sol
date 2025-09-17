3c6874849b057d58a9faf9a0834ce9ef85852a60
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-09-17 20:19:37 +0530

    Time: 0 ms (100%), Space: 15.3 MB (17.96%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,30 +11,50 @@
  */
 class Solution {
 public:
+    vector<vector<int>> ans;
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(!root)return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        int i=0;
-        while(!q.empty()){
-           int sz=q.size();
-           vector<int>v(sz);
-           int j=0,n=sz;
-           while(sz--){
-               TreeNode * f=q.front();
-               q.pop();
-               if(i%2==0)
-               v[j]=f->val;
-               else v[n-j-1]=f->val;
-               j++;
-               if(f->left)q.push(f->left);
-               if(f->right)q.push(f->right);
-           }
-           i++;
-           ans.push_back(v);
-
+        
+        if(root == NULL){
+            return ans; 
         }
-        return ans;
+
+        queue<TreeNode*> q; 
+        q.push(root);
+        q.push(NULL);
+
+        int flag = 0;
+
+        vector<int> curr; 
+
+        while(!q.empty()){
+            TreeNode* t = q.front(); 
+            q.pop();
+
+            if(t == NULL){
+                if(flag == 1){
+                    reverse(curr.begin(), curr.end());
+                }
+
+                ans.push_back(curr);
+                curr.clear(); 
+
+                if(!q.empty()){
+                    // not the last level
+                    q.push(NULL);
+                }
+
+                flag = !flag;
+
+            }else{
+                curr.push_back(t->val);
+
+                // push the next children
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            
+            }
+        }
+
+        return ans; 
     }
 };
