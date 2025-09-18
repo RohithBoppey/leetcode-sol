f65ab714bc7f0f8156ff5ec8d973ffb6523ce185
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2025-09-18 11:42:24 +0530

    Time: 0 ms (100%), Space: 21.9 MB (46.25%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,57 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
+    bool solve(TreeNode* root, long mn, long mx){
+        if(root == NULL) return true; 
+        if(root->val <= mn || root->val >= mx) return false;
 
-    bool solve(TreeNode* root, long& l, long& h){
-
-        if(root == NULL){
-            return true;
-        }
-
-        long val = root->val;
-        // check if the selected node is under limits or not
-        int valid = (val >= l && val <= h);
-        if(!valid){
-            // not present in limits
-            return false;
-        }
-
-        // the node itself is valid
-        // check for left and right
-        int temp = h;
-        h = val - 1;
-
-        if(!solve(root->left, l, h)){
-            return false;
-        }
-        h = temp;
-
-        temp = l;
-        l = val + 1;
-        if(!solve(root->right, l, h)){
-            return false;
-        }
-        l = temp;
-
-        return true;
-
+        return solve(root->left, mn, root->val) && solve(root->right, root->val, mx);
     }
 
     bool isValidBST(TreeNode* root) {
-        
-        // set limits for a certain node appraoch
-        long low = INT_MIN, high = INT_MAX;
-        
-        // if there is a single node, allow it
-        if(!root->left && !root->right){
-            // singe node
-            return true;
-        }
-
-        return solve(root, low, high);
-
+        return solve(root, LONG_MIN, LONG_MAX);
     }
 };
