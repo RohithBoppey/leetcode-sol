bb5731a13310737db1536d3f970d5be9928e7621
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2026-04-20 10:35:18 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.41%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -1,20 +1,17 @@
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
-        # start from left and find the distinct color from the right 
         n = len(colors)
-        ans = 0
+        res = 0
         
-        # with caching 
-        ch = {}
+        # Case 1: Distance from the first house to the furthest different color from the right
+        for j in range(n - 1, 0, -1):
+            if colors[0] != colors[j]:
+                res = max(res, j)
+                break
+                
+        # Case 2: Distance from the last house to the furthest different color from the left
         for i in range(0, n - 1):
-            if ch.get(i) is not None: 
-                continue 
-            c = colors[i]
-            j = n - 1
-            while j > i and colors[j] == c:
-                j -= 1
-            # now j points to an index which is having a different color than i
-            ans = max(ans, j - i)
-            ch[i] = ch.get(i, 0) + 1
-
-        return ans
+            if colors[n - 1] != colors[i]:
+                res = max(res, n - 1 - i)
+                break 
+        return res
