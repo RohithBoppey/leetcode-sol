923ef1a6c2d506fe7ee8f4dad6d354cf012f402b
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2026-04-20 10:29:06 +0530

    Time: 0 ms (100%), Space: 19.2 MB (91.81%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -3,11 +3,18 @@ class Solution:
         # start from left and find the distinct color from the right 
         n = len(colors)
         ans = 0
-        for i in range(0, n): 
+        
+        # with caching 
+        ch = {}
+        for i in range(0, n - 1):
+            if ch.get(i) is not None: 
+                continue 
             c = colors[i]
             j = n - 1
             while j > i and colors[j] == c:
                 j -= 1
             # now j points to an index which is having a different color than i
             ans = max(ans, j - i)
+            ch[i] = ch.get(i, 0) + 1
+
         return ans
