159a2a64b9b6020bf450d623e34ac247c10c7a8e
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2026-04-21 11:13:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        n = len(nums)
+        cnt = [0]*(10**5 + 1)
+
+        for i in nums:
+            cnt[i] += 1
+
+        steps = 0
+
+        # can iterate on cnt array
+        for i in range(len(cnt)): 
+            c = cnt[i]
+            dup = 0
+            if c != 0: 
+                dup = c - 1
+
+            # duplicates needs to be incremented/subtracted
+            steps += dup 
+            
+            # now that many dups move to the next step
+            if i + 1 < len(cnt): 
+                cnt[i + 1] += dup
+
+        return steps
