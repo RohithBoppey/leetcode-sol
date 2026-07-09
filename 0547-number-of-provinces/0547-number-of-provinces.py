31df1ce22a9e82d68aa2c04d4e9c81e4a3da795d
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2026-07-09 17:01:52 +0530

    Time: 15 ms (11.75%), Space: 20.4 MB (69.7%) - LeetHub - Time: O(n^2 * α(n)), Space: O(n)

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,23 +1,57 @@
-def solve(i, n, vis, conn): 
-    vis[i] = 1 
-    # push all neighbours 
-    for j in range(n): 
-        if i != j and conn[i][j] == 1 and vis[j] == 0: 
-            # connection there, and the children is not visited
-            solve(j, n, vis, conn)
+class DSU: 
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1 for _ in range(n)]
+
+    def find(self, x): 
+        while (self.parent[x] != x):
+            x = self.parent[x]
+        return x
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def union(self, x, y):
+        a = self.find(x)
+        b = self.find(y)
+
+        if a == b: 
+            return 
+
+        if self.size[a] < self.size[b]: 
+            # merge a into b
+            self.parent[a] = b
+            self.size[b] += self.size[a]
+        else:
+            self.parent[b] = a
+            self.size[a] += self.size[b]
 
 
 class Solution:
     def findCircleNum(self, conn: List[List[int]]) -> int:
-        # do dfs
-        ans, n = 0, len(conn)
-        vis = [0] * n
+        # solving using DSU
+        
+        # # test
+        # dsu = DSU(5)
+        # dsu.union(0,1)
+        # dsu.union(1,2)
+        # print(dsu.connected(0,2))
+        # print(dsu.connected(0,3))
+
+        n = len(conn)
+        dsu = DSU(n)
 
         for i in range(n):
-            if vis[i] == 0: 
-                # not visited
-                solve(i, n, vis, conn)
-                ans += 1
+            for j in range(n): 
+                if i != j and conn[i][j] == 1: 
+                    # unify into a component
+                    dsu.union(i, j)
+
+        # now to update the parent linearly
+        ans = 0
+        for i in range(n):
+            x = dsu.find(i)
+            ans += 1 if x == i else 0
 
         return ans
 
