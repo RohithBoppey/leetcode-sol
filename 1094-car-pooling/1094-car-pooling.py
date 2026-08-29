ea7f62966e56fed47c5c07099d481676d4474056
Rohith Boppey <73538974+RohithBoppey@users.noreply.github.com>
2026-08-29 20:13:06 +0530

    Time: 0 ms (100%), Space: 19.5 MB (83.01%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,20 +1,21 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        final = []
+        dp = [0]*1001
+        n = 0
+
         for i in trips: 
-            final.append([i[1], i[0]])
-            final.append([i[2], -i[0]])
-        
-        final.sort(key = lambda x : (x[0], x[1])) 
-        # print(final)
-        curr = 0 
-        for i in final: 
-            curr += i[1]
+            dp[i[1]] += i[0]
+            dp[i[2]] += -i[0]
+            n = max(i[2], n)
+
+        curr = 0
+        for i in range(n + 1): 
+            curr += dp[i]
             if curr < 0 or curr > capacity:
                 return False
 
-        return True
-
+        # print(dp)
+        return curr >= 0
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
